3606b77ff3c05e0abc0a83716671e523b0b97ff9
Your Name <you@example.com>
2025-02-21 20:03:00 +0100

    test code

---
+++ View/ProcessPageView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace KitBox_Project.View
+{
+    public partial class ProcessPageView : UserControl
+    {
+        public ProcessPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
