38fce2948d460ed9c92f9fbb5023fce517f2fc50
Onya Pierrot <22233@ecam.be>
2025-02-26 00:36:47 +0100

    Test page dyna

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -7,5 +7,21 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        var textBlock = new TextBlock
+        {
+           Text = "Bikass La Menace" ,
+           HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+           VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+        };
+        this.Content = textBlock;
+    }
+
+    private static Grid CreateGrid(){
+        var grid = new Grid();
+        grid.ColumnDefinitions.Add(new ColumnDefinition(100));
+        grid.ColumnDefinitions.Add(new ColumnDefinition(100));
+        grid.RowDefinitions.Add(new RowDefinition(100));
+        grid.RowDefinitions.Add(new RowDefinition(100));
+        return grid; 
     }
 }
