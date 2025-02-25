1ee41ff944a9124896e2e001f462a962414d2117
Your Name <you@example.com>
2025-02-25 15:29:40 +0100

    Test menu + bouton

---
+++ View/HomePage.axaml.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KitBox_Project.View;
+
+public partial class HomePage : UserControl
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -17,19 +17,5 @@ namespace KitBox_Project.ViewModel
             get => _title;
             set => this.RaiseAndSetIfChanged(ref _title, value); // Grâce au binding et à RaiseAndSetIfChanged(), si la valeur de Title change dans le ViewModel, la View (UI) sera mise à jour automatiquement, sans qu'il soit nécessaire d'écrire du code supplémentaire pour rafraîchir l'affichage (chargé par le binding).
         }
-
-        public ReactiveCommand<Unit, Unit> ShowMessageCommand { get; }
-
-        public MainViewModel()
-        {
-            // Initialisation de la commande avec une action qui affiche un message
-            ShowMessageCommand = ReactiveCommand.CreateFromTask(ShowMessage);
-        }
-
-        private async Task ShowMessage()
-        {
-            var messageBox = MessageBoxManager.GetMessageBoxStandard("Information", "Bouton cliqué !", ButtonEnum.Ok, Icon.Info);
-            await messageBox.ShowAsync();
-        }
     }
 } 
